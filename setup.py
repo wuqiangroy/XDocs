2f680db07e4c8709c796a5a296b1e5f4ef1d9ae6
gaojiuli <gaojiuli@gmail.com>
2017-01-11 16:42:55 +0800

    setup.py update

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name="xdocs",
     version="0.0.1",
+    license='MIT',
+    description='Documentation drives developing.',
+    author='gaojiuli',
+    author_email='gaojiuli@gmail.com',
     install_requires=["Click", "bottle"],
     entry_points="""
         [console_scripts]
