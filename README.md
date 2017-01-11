78369129568e9662399ebe3a757d0bb7e1337ebf
gaojiuli <gaojiuli@gmail.com>
2017-01-11 17:51:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+# Xdocs
+
+RESTful API documents for humans
+
+mock server based on python
+document client based on javascript
+
+## Getting Started
+
+```
+pip install xdocs
+xdocs new tutorial
+cd tutorial
+xdocs run
+```
+
+## Feature Support
+
+1. yml语法编写接口文档
+2. 文档写法符合RESTful的要求
+3. 自动生成优雅的web页面
+4. 自动生成mock数据服务器
+5. 文档修改热更新
+
+## TODO
+
+1. 设计文档结构
+2. 设计文档内容规范 
+3. 实现根据文档翻译为HTML
+4. 实现根据文档生成mock数据接口
+
+## Final Thoughts
+
+文档驱动开发
+
+## License
+
+Xdocs is released under the [MIT license](LICENSE).
--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-from setuptools import setup
+import os
+
+from setuptools import setup, find_packages
+
 
 setup(
     name="xdocs",
-    version="0.0.3",
+    version="0.0.5",
     license='MIT',
     description='Documentation drives developing.',
     author='gaojiuli',
     author_email='gaojiuli@gmail.com',
     install_requires=["Click", "bottle"],
     url="https://github.com/gaojiuli/Xdocs",
-    packages=['Xdocs'],
+    include_package_data=True,
+    packages=find_packages(),
     entry_points="""
         [console_scripts]
         xdocs=Xdocs.server.xdocs:cli
