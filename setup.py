f97024963e660e5640e3382bb89e53e0f5e6bad0
gaojiuli <gaojiuli@gmail.com>
2017-01-11 17:35:32 +0800

    init

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name="xdocs",
-    version="0.0.1",
+    version="0.0.3",
     license='MIT',
     description='Documentation drives developing.',
     author='gaojiuli',
     author_email='gaojiuli@gmail.com',
     install_requires=["Click", "bottle"],
+    url="https://github.com/gaojiuli/Xdocs",
+    packages=['Xdocs'],
     entry_points="""
         [console_scripts]
         xdocs=Xdocs.server.xdocs:cli
