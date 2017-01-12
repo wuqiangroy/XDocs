e6e4a34fc4f13483572ceb2dcabf2e410ca1736b
gaojiuli <gaojiuli@gmail.com>
2017-01-12 10:08:16 +0800

    init

---
+++ xdocs/__main__.py
@@ -0,0 +1,25 @@
+import click
+
+
+@click.group(context_settings={'help_option_names': ['-h', '--help']})
+def cli():
+    """
+    XDocs - 文档驱动开发
+    """
+
+
+@cli.command(name="new")
+@click.argument('name')
+def new(name):
+    """Create a new XDocs project"""
+    click.echo('Project %s!' % name)
+
+
+@cli.command(name="run")
+def run():
+    """Run the builtin development server"""
+    click.echo('the serve is running')
+
+
+if __name__ == '__main__':
+    cli()
