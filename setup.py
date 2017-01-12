6e069c5622c2ca5dabcb85ce53bd1ab18a27fce2
gaojiuli <gaojiuli@gmail.com>
2017-01-12 10:08:05 +0800

    init

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="xdocs",
-    version="0.0.5",
+    name='xdocs',
+    version='0.0.5',
     license='MIT',
     description='Documentation drives developing.',
     author='gaojiuli',
     author_email='gaojiuli@gmail.com',
-    install_requires=["click", "bottle"],
-    url="https://github.com/gaojiuli/Xdocs",
-    # include_package_data=True,
+    install_requires=["click", "bottle", 'PyYAML>=3.10'],
+    url='https://github.com/gaojiuli/Xdocs',
     packages=find_packages(),
     package_data={
         'client': ['*'],
         'examples': ['*']
     },
-    entry_points="""
-        [console_scripts]
-        xdocs=Xdocs.server.xdocs:cli
-    """
+    entry_points={
+        'console_scripts': [
+            'xdocs=xdocs.__main__:cli',
+        ],
+    },
 )
