de52d1b0bdb03d0e6fcd854ac6e342d490726d21
xm <xia0ming@foxmail.com>
2017-01-13 12:21:02 +0800

    init

--- xdocs/client/static/xdocs.js
+++ xdocs/client/static/xdocs.js
@@ -1,3 +1,14 @@
-function ddd() {
-    return 1
-}
+const nativeObject = [];
+
+
+YAML.load('../../docs/Todo.yml', function(result) {
+    nativeObject[0] = result;
+    console.log(nativeObject[0]);
+});
+
+var docs = new Vue({
+    el: '#docs',
+    data: {
+        doc_array: nativeObject
+    }
+})
