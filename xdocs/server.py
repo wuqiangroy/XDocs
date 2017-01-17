9689293180c254853b9702041fbc890189eaaf7f
gaojiuli <gaojiuli@gmail.com>
2017-01-17 16:47:17 +0800

    add mock data server

--- xdocs/server.py
+++ xdocs/server.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 from bottle import static_file, Bottle, abort, request
 from faker import Factory
@@ -27,10 +28,12 @@ def generate_mock_data(file_type):
 def setup_data(resources):
     for k, v in resources.items():
         app.data[k] = {}
-        temp = {}
-        for field in v['model']:
-            temp[field["field"]] = generate_mock_data("name")
-        app.data[k][temp["id"]] = temp
+        for i in range(20):
+            temp = {}
+            for field in v['model']:
+                temp[field["field"]] = generate_mock_data("name")
+            temp['id'] = uuid.uuid4().__str__()
+            app.data[k][temp["id"]] = temp
 
 
 setup_data(app.resources)
@@ -66,25 +69,41 @@ def static(filename):
 
 @app.route('/api/<resources>/', method='GET')
 def list(resources):
-    try:
-        results = []
-        for data in app.data[resources].values():
-            return_fields = app.resources[resources]['action']['list']['return']
-            results.append(dict_filter_by_array(data, return_fields))
-        return {'data': results}
-    except:
-        abort(404)
+    results = []
+    limit = int(request.query.get('limit', 10))
+    offset = int(request.query.get('offset', 0))
+    for data in app.data[resources].values():
+        return_fields = app.resources[resources]['action']['list']['return']
+        results.append(dict_filter_by_array(data, return_fields))
+    next = None
+    previous = None
+
+    if offset + limit <= len(app.data[resources]):
+        next = "http://{}/api/{}/?limit={}&offset={}".format(request.get_header('host'),
+                                                                      resources,
+                                                                      limit,
+                                                                      offset + limit)
+    if offset - limit >= 0:
+        previous = "http://{}/api/{}/?limit={}&offset={}".format(request.get_header('host'),
+                                                                          resources,
+                                                                          limit,
+                                                                          offset - limit)
+    return {'count': len(app.data[resources]),
+            "next": next,
+            "previous": previous,
+            'results': results[offset:(offset + limit)]}
 
 
 @app.route('/api/<resources>/', method='POST')
 def create(resources):
     try:
         new_data = dict.fromkeys([field['field'] for field in app.resources[resources]["model"]])
-        print(request.POST)
+        new_data['id'] = uuid.uuid4().__str__()
         for key in new_data:
-            if key in request.POST:
-                new_data[key] = request.POST[key]
-        return {'data': new_data}
+            if key in request.json:
+                new_data[key] = request.json.get(key)
+        app.data[resources][new_data['id']] = new_data
+        return new_data
     except:
         abort(404)
 
@@ -92,7 +111,7 @@ def create(resources):
 @app.route('/api/<resources>/<resource_id>/', method='GET')
 def retrieve(resources, resource_id):
     try:
-        return {'data': app.data[resources][resource_id]}
+        return app.data[resources][resource_id]
     except:
         abort(404)
 
@@ -100,8 +119,9 @@ def retrieve(resources, resource_id):
 @app.route('/api/<resources>/<resource_id>/', method='PUT')
 def replace(resources, resource_id):
     try:
-        del app.data[resources][resource_id]
-        return {'detail': 'success'}
+        for k, v in app.data[resources][resource_id].items():
+            app.data[resources][resource_id][k] = request.json.get(k)
+        return app.data[resources][resource_id]
     except:
         abort(404)
 
@@ -109,8 +129,9 @@ def replace(resources, resource_id):
 @app.route('/api/<resources>/<resource_id>/', method='PATCH')
 def update(resources, resource_id):
     try:
-        del app.data[resources][resource_id]
-        return {'detail': 'success'}
+        for k, v in app.data[resources][resource_id].items():
+            app.data[resources][resource_id][k] = request.json.get(k, v)
+        return app.data[resources][resource_id]
     except:
         abort(404)
 
