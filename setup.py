5fb39877a6f1bc197f6b5ca2d403e47f79940861
gaojiuli <gaojiuli@gmail.com>
2017-01-19 11:02:36 +0800

    pypi setup config

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import setup, find_packages
 
+long_description = (
+    "XDocs is a fast and simple documentation generator "
+    "that's geared towards building project documentation. Documentation "
+    "source files are written in YAML, and configured with a single YAML "
+    "configuration file."
+)
 setup(
     name='xdocs',
-    version='0.2.1',
+    version='0.2.2',
     license='MIT',
     description='Documentation drives developing.',
+    long_description=long_description,
     author='gaojiuli',
     author_email='gaojiuli@gmail.com',
     include_package_data=True,
     install_requires=[
-        'faker',
-        "click",
-        'livereload',
-        'bottle',
-        'PyYAML'],
+        'Faker>=0.7.7',
+        "click>=6.7",
+        'livereload>=2.5.1',
+        'bottle>=0.12.13',
+        'PyYAML>=3.12'],
     url='https://github.com/gaojiuli/XDocs',
     packages=find_packages(),
     entry_points={
@@ -21,4 +28,24 @@ setup(
             'xdocs=xdocs.__main__:cli',
         ],
     },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        "Programming Language :: Python :: Implementation :: CPython",
+        'Topic :: Documentation',
+        'Topic :: Text Processing',
+    ],
+    zip_safe=False,
 )
